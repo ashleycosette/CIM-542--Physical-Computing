4d3b8ba37f8d93e122e8154f0d3476c1a6944846
Ashley Levine <ashleylevine@Ashleys-MBP-3.attlocal.net>
2019-03-31 21:11:27 -0400

    midterm

--- Midterm/Midterm/Midterm.ino
+++ Midterm/Midterm/Midterm.ino
@@ -1,14 +1,14 @@
-#include <Servo.h>
+ #include <Servo.h>
 
 int switchPin = 2;
-int led1 = 3;
-int led2 = 4;
+//int led1 = 3;
+//int led2 = 4;
 
 int switchRead = 0;
 
 int counter = 0;
 
-boolean wasPressed = false;
+boolean wasPressed = false; 
 
 long prevMillis = 0;
 int interval = 0;
@@ -21,10 +21,19 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(switchPin, INPUT);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
+//  pinMode(led1, OUTPUT);
+//  pinMode(led2, OUTPUT);
+  servo1.attach(3);
+  servo2.attach(4);
+  
+
+//understanding the movement
+//  servo1.write(0);
+//  delay(1000);
+//  servo1.write(180);
 
 
+ 
 }
 
 void loop() {
@@ -40,7 +49,9 @@ void loop() {
   }
 
   if (switchRead == LOW && wasPressed == true) {
+   // resetting the press
     wasPressed = false;
+    //action
     counter = counter + 1;
   }
 
@@ -48,14 +59,19 @@ void loop() {
   Serial.println(counter);
 
   if (counter == 0) {
-    servo.write (180)
+    servo1.write(90);
+    servo2.write(0);
+    //servo2 write 180
    //open servo
 
   } else if (counter == 1) {
-    servo.write (0)
+    servo1.write(0);
+    servo2.write(90);
      //close
   } else if (counter == 2) {
     //reset back to 0
+    counter = 0;
+    
   }
 
 }
